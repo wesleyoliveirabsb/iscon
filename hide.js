c9e004b86cfc763dd72c79a275ac5e3d56042818
Wesley Oliveira Silva <will.lg@hotmail.com>
2024-05-20 18:29:31 -0300

    progess

---
+++ hide.js
@@ -0,0 +1,30 @@
+
+function hide(){
+    document.getElementById('card1').style.transform = "translateY(-150px)"
+
+    document.getElementById('card2').style.backgroundColor='#000000';
+    document.getElementById('card2').style.opacity='0.8';
+
+    document.getElementById('card3').style.backgroundColor='#000000';
+    document.getElementById('card3').style.opacity='0.8';
+}
+
+function hide2(){
+    document.getElementById('card2').style.transform = "translateY(-150px)"
+
+    document.getElementById('card1').style.backgroundColor='#000000';
+    document.getElementById('card1').style.opacity='0.8';
+
+    document.getElementById('card3').style.backgroundColor='#000000';
+    document.getElementById('card3').style.opacity='0.8';
+}
+
+function hide3(){
+    document.getElementById('card3').style.transform = "translateY(-150px)"
+
+    document.getElementById('card1').style.backgroundColor='#000000';
+    document.getElementById('card1').style.opacity='0.8';
+
+    document.getElementById('card2').style.backgroundColor='#000000';
+    document.getElementById('card2').style.opacity='0.8';
+}
---
+++ show.js
@@ -0,0 +1,14 @@
+
+function show(){
+    document.getElementById('card1').style.transform = "translateY(0)"
+    document.getElementById('card1').style.backgroundColor='#737495';
+    document.getElementById('card1').style.opacity='1';
+
+    document.getElementById('card2').style.transform = "translateY(0)"
+    document.getElementById('card2').style.backgroundColor='#68A8AD';
+    document.getElementById('card2').style.opacity='1';
+
+    document.getElementById('card3').style.transform = "translateY(0)"
+    document.getElementById('card3').style.backgroundColor='#6C8672';
+    document.getElementById('card3').style.opacity='1';
+}
